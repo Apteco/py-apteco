ec983bb0bf84c67b08bef6ab0637be1b409cfdae
Tim Morris <tim.morris@apteco.com>
2020-06-25 16:14:55 +0100

    Move system table fixtures for integration tests into conftest.py. [AB#3490] - Change should have been made already, and Data Grid integration tests will need to use fixtures as well.

--- tests/integration_tests/conftest.py
+++ tests/integration_tests/conftest.py
@@ -17,3 +17,38 @@ def holidays():
         credentials["user"],
         credentials["password"],
     )
+
+
+@pytest.fixture(scope="session")
+def people(holidays):
+    return holidays.tables["People"]
+
+
+@pytest.fixture(scope="session")
+def bookings(holidays):
+    return holidays.tables["Bookings"]
+
+
+@pytest.fixture(scope="session")
+def households(holidays):
+    return holidays.tables["Households"]
+
+
+@pytest.fixture(scope="session")
+def policies(holidays):
+    return holidays.tables["Policies"]
+
+
+@pytest.fixture(scope="session")
+def web_visits(holidays):
+    return holidays.tables["WebVisits"]
+
+
+@pytest.fixture(scope="session")
+def communications(holidays):
+    return holidays.tables["Communications"]
+
+
+@pytest.fixture(scope="session")
+def responses(holidays):
+    return holidays.tables["Responses Attributed"]
--- tests/integration_tests/query_integration_test.py
+++ tests/integration_tests/query_integration_test.py
@@ -17,41 +17,6 @@ from apteco.query import (
 )
 
 
-@pytest.fixture(scope="session")
-def people(holidays):
-    return holidays.tables["People"]
-
-
-@pytest.fixture(scope="session")
-def bookings(holidays):
-    return holidays.tables["Bookings"]
-
-
-@pytest.fixture(scope="session")
-def households(holidays):
-    return holidays.tables["Households"]
-
-
-@pytest.fixture(scope="session")
-def policies(holidays):
-    return holidays.tables["Policies"]
-
-
-@pytest.fixture(scope="session")
-def web_visits(holidays):
-    return holidays.tables["WebVisits"]
-
-
-@pytest.fixture(scope="session")
-def communications(holidays):
-    return holidays.tables["Communications"]
-
-
-@pytest.fixture(scope="session")
-def responses(holidays):
-    return holidays.tables["Responses Attributed"]
-
-
 def test_selector_operators(bookings, people, households):
     sweden = bookings["boDest"] == "29"
     assert sweden.count() == 25_207
