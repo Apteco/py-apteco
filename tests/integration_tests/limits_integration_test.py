c5c97d48299c49a538945680e534829cad26246b
Thomas Campbell <thomas.campbell@tanti.org.uk>
2020-09-15 11:59:53 +0100

    Rewrites tests for limits methods. [AB#322, AB#323, AB#324, AB#325]

--- tests/integration_tests/limits_integration_test.py
+++ tests/integration_tests/limits_integration_test.py
@@ -1,5 +1,6 @@
 import pytest
 from apteco.query import LimitClause
+from fractions import Fraction
 
 
 @pytest.fixture()
@@ -14,60 +15,52 @@ def france(bookings):
 
 @pytest.mark.xfail(reason="Limits under development")
 def test_limit(men):
-    men_skip_5_first_10pct = men.limit(
-        limit_type="first", value_type="percent", value=10, skip_first=5
-    )
-    men_regular_100 = men.limit(limit_type="regular", value_type="total", value=100)
-    men_random_two_thirds = men.limit(
-        limit_type="random", value_type="fraction", value=(2, 3)
-    )
+    men_10 = men.sample(n=100)
+    men_skip_5_first_10pct = men.sample(frac=0.1, skip_first=5)
+    men_regular_100 = men.sample(n=100, sample_type="regular")
+    men_random_two_thirds = men.sample(frac=Fraction(2, 3), sample_type="random")
+    assert men_10.count() == 10
     assert men_skip_5_first_10pct.count() == 37857
     assert men_regular_100.count() == 100
     assert men_random_two_thirds.count() == 252377
 
 
 @pytest.mark.xfail(reason="Top N under development")
-def test_top_n(men):
-    men_top_100_by_income = men.top_n(
-        variable="income", value_type="total", direction="top", between=False, value=100
+def test_top_n(men, people):
+    men_top_100_by_income = men.limit(n=100, by=people["Income"])
+    men_between_bottom_5pct_10pct_by_income = men.limit(
+        frac=(0.05, 0.1),
+        by=people["Income"],
+        ascending=True,
     )
-    men_between_bottom_5pct_10pct_by_income = men.top_n(
-        variable="income",
-        value_type="percent",
-        direction="bottom",
-        between=True,
-        value=(5, 10),
-    )
-    men_between_top_20_50_by_income = men.top_n(
-        variable="income",
-        value_type="total",
-        direction="top",
-        between=True,
-        value=(20, 50),
-    )
+    men_between_top_20_50_by_income = men.limit(n=(20, 50), by=people["Income"])
     assert men_top_100_by_income.count() == 100
     assert men_between_bottom_5pct_10pct_by_income.count() == 18928
     assert men_between_top_20_50_by_income.count() == 31
 
 
 @pytest.mark.xfail(reason="N per variable under development")
-def test_n_per_variable(men):
-    men_highest_5_income_per_surname = men.n_per_variable(
-        value=5, grouping_variable="surname", sequence_variable="income", top=True
+def test_n_per_variable(men, people):
+    men_highest_5_income_per_surname = men.limit(
+        value=5, by=people["Income"], per=people["Surname"]
     )
-    men_10_per_surname = men.n_per_variable(value=10, grouping_variable="surname")
+    men_10_per_surname = men.limit(n=10, per=people["Surname"])
+    men_lowest_2_income_per_surname = men.limit(
+        n=2, by=people["Income"], ascending=True, per=people["Surname"]
+    )
     assert men_highest_5_income_per_surname.count() == 117264
     assert men_10_per_surname.count() == 148541
+    assert men_lowest_2_income_per_surname.count() == 81527
 
 
 @pytest.mark.xfail(reason="N per table under development")
-def test_n_per_table(france):
-    france_bookings_1_per_person = france.n_per_table(value=1, grouping_table="people")
-    france_bookings_highest_2_costs_per_person = france.n_per_table(
-        value=2, grouping_table="people", top=True, sequence_variable="cost"
+def test_n_per_table(france, people, bookings, households):
+    france_bookings_1_per_person = france.limit(n=1, per=people)
+    france_bookings_highest_2_costs_per_person = france.limit(
+        n=2, by=bookings["Cost"], per=people
     )
-    france_bookings_lowest_3_profits_per_household = france.n_per.table(
-        value=3, grouping_table="households", top=False, sequence_variable="profit"
+    france_bookings_lowest_3_profits_per_household = france.limit(
+        n=3, by=bookings["Profit"], ascending=True, per=households
     )
     assert france_bookings_1_per_person.count() == 796794
     assert france_bookings_highest_2_costs_per_person.count() == 926322
