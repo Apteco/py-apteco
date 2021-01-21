81da2d26c2786c9208c1d4a354e75bead7768cf8
Tim Morris <tim.morris@apteco.com>
2021-01-21 11:41:38 +0000

    Refactor Sum and Mean to inherit from BaseStatistic class. [AB#3522]

--- src/apteco/statistics.py
+++ src/apteco/statistics.py
@@ -1,33 +1,27 @@
 import apteco_api as aa
 
 
-class Sum:
+class BaseStatistic:
     def __init__(self, operand):
+        self.table = operand.table
         self.operand = operand
 
-        self.name = f"Sum({self.operand.description})"
-        self.table = operand.table
+        self.name = f"{self._model_function}({self.operand.description})"
 
     def _to_model_measure(self, table):
         return aa.Measure(
             id=self.name,
             resolve_table_name=table.name,
-            function="Sum",
+            function=self._model_function,
             variable_name=self.operand.name,
         )
 
 
-class Mean:
-    def __init__(self, operand):
-        self.operand = operand
+class Sum(BaseStatistic):
+    _model_function = "Sum"
 
-        self.name = f"Mean({self.operand.description})"
-        self.table = operand.table
 
-    def _to_model_measure(self, table):
-        return aa.Measure(
-            id=self.name,
-            resolve_table_name=table.name,
-            function="Mean",
-            variable_name=self.operand.name,
-        )
+class Mean(BaseStatistic):
+    _model_function = "Mean"
+
+
