bc9422a1d59864c5837f353f6c910c5665be556d
Tim Morris <tim.morris@apteco.com>
2021-01-22 10:26:41 +0000

    Add input validation for operand on statistics. [AB#3522] - rename BaseStatistic to Statistic

--- src/apteco/statistics.py
+++ src/apteco/statistics.py
@@ -1,9 +1,21 @@
 import apteco_api as aa
 
 
-class BaseStatistic:
+def _ensure_correct_type(operand, accepted_types):
+    try:
+        if operand.type in accepted_types:
+            return
+    except AttributeError:
+        pass
+    raise ValueError(
+        f"The operand must be a" f" {' or '.join(t for t in accepted_types)} variable"
+    ) from None
+
+
+class Statistic:
     def __init__(self, operand, *, label=None):
         self.table = operand.table
+        _ensure_correct_type(operand, self._accepted_types)
         self.operand = operand
 
         self.label = label
@@ -19,73 +31,85 @@ class BaseStatistic:
         )
 
 
-class Sum(BaseStatistic):
+class Sum(Statistic):
     _model_function = "Sum"
     _display_name = "Sum"
+    _accepted_types = ("Numeric",)
 
 
-class Mean(BaseStatistic):
+class Mean(Statistic):
     _model_function = "Mean"
     _display_name = "Mean"
+    _accepted_types = ("Numeric",)
 
 
-class Populated(BaseStatistic):
+class Populated(Statistic):
     _model_function = "VariableCount"
     _display_name = "Populated"
+    _accepted_types = ("Numeric",)
 
 
-class Min(BaseStatistic):
+class Min(Statistic):
     _model_function = "Minimum"
     _display_name = "Min"
+    _accepted_types = ("Numeric",)
 
 
-class Max(BaseStatistic):
+class Max(Statistic):
     _model_function = "Maximum"
     _display_name = "Max"
+    _accepted_types = ("Numeric",)
 
 
-class Median(BaseStatistic):
+class Median(Statistic):
     _model_function = "Median"
     _display_name = "Median"
+    _accepted_types = ("Numeric",)
 
 
-class Mode(BaseStatistic):
+class Mode(Statistic):
     _model_function = "Mode"
     _display_name = "Mode"
+    _accepted_types = ("Numeric", "Selector")
 
 
-class Variance(BaseStatistic):
+class Variance(Statistic):
     _model_function = "Variance"
     _display_name = "Variance"
+    _accepted_types = ("Numeric",)
 
 
-class StdDev(BaseStatistic):
+class StdDev(Statistic):
     _model_function = "StandardDeviation"
     _display_name = "Std Dev"
+    _accepted_types = ("Numeric",)
 
 
-class LowerQuartile(BaseStatistic):
+class LowerQuartile(Statistic):
     _model_function = "LowerQuartile"
     _display_name = "Lower Quartile"
+    _accepted_types = ("Numeric",)
 
 
-class UpperQuartile(BaseStatistic):
+class UpperQuartile(Statistic):
     _model_function = "UpperQuartile"
     _display_name = "Upper Quartile"
+    _accepted_types = ("Numeric",)
 
 
-class InterQuartileRange(BaseStatistic):
+class InterQuartileRange(Statistic):
     _model_function = "InterQuartileRange"
     _display_name = "Inter Quartile Range"
+    _accepted_types = ("Numeric",)
 
 
-class CountDistinct(BaseStatistic):
+class CountDistinct(Statistic):
     _model_function = "CountDistinct"
     _display_name = "Count Distinct"
+    _accepted_types = ("Numeric", "Selector")
 
 
-class CountMode(BaseStatistic):
+class CountMode(Statistic):
     _model_function = "MaxDistinctCount"
     _display_name = "Count Mode"
-
-
+    _accepted_types = ("Numeric", "Selector")
