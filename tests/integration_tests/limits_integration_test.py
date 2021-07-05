1041b7e71be57abd6b3f91ea6abc3567f4c553b6
Tim Morris <tim.morris@apteco.com>
2021-07-05 19:15:47 +0100

    Add 'mixed' integration tests for LimitClause (amend 89a15ad). [AB#322]

--- tests/integration_tests/limits_integration_test.py
+++ tests/integration_tests/limits_integration_test.py
@@ -284,6 +284,8 @@ class TestLimitClause:
         * numeric input type (total, percent, fraction)
         * skip_first given or not
 
+    Some general "mixed" tests at the end.
+
     """
     def test_first_n(self, holidays, france):
         france_first_100 = LimitClause(france, 100, session=holidays)
@@ -406,6 +408,30 @@ class TestLimitClause:
             session=holidays,
         )
         assert france_random_12_51sts_skip_678.count() == 123406
+
+    def test_limit_clause_and(self, men, holidays, people):
+        men_first_100 = LimitClause(men, 100, session=holidays)
+        assert men_first_100.count() == 100
+        students_men_first_100 = men_first_100 & (people["Occupation"] == "4")
+        assert students_men_first_100.count() == 9
+
+    def test_limit_clause_or(self, holidays, bookings):
+        sweden = bookings["Destination"] == "29"
+        sweden_1_20th_regular = LimitClause(sweden, fraction=Fraction(1, 20), sample_type="Stratified", session=holidays)
+        assert sweden_1_20th_regular.count() == 1261
+        cost_gt_100 = bookings["Cost"] > 100
+        cost_gt_100_first_0_38_pct = LimitClause(cost_gt_100, percent=0.38, session=holidays)
+        assert cost_gt_100_first_0_38_pct.count() == 8002
+        limit_or_clause = sweden_1_20th_regular | cost_gt_100_first_0_38_pct
+        assert limit_or_clause.count() == 9258
+
+    def test_limit_clause_of_limit_clause(self, holidays, households, people):
+        south_west = households["Region"] == "04"
+        assert south_west.count() == 77139
+        south_west_1_456th = LimitClause(south_west, fraction=Fraction(1, 456), session=holidays)
+        assert south_west_1_456th.count() == 170
+        south_west_1_456th_78_9pct = LimitClause(south_west_1_456th, percent=78.9, session=holidays)
+        assert south_west_1_456th_78_9pct.count() == 135
 
 
 def verify_topn_values(
