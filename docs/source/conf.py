ec6710101ee56c58a1033f45250006371a172f99
Adam Robertson <adam.robertson@apteco.com>
2026-01-07 17:36:06 +0000

    TASK | Adam | Update 2026 copyright notice

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'py-apteco'
-copyright = '2025, Apteco Ltd'
+copyright = '2026, Apteco Ltd'
 author = 'Apteco Ltd'
 
 # The short X.Y version
